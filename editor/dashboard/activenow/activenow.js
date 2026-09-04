41e9a9dca96fe83a887e91ab63b94833b7e65e14
Eric Francois <eric.francois@uni.lu>
2026-09-04 17:55:25 +0200

    Uncommented another line lost in 3.6.3 update

--- editor/dashboard/activenow/activenow.js
+++ editor/dashboard/activenow/activenow.js
@@ -339,7 +339,7 @@ export default class ActiveNow {
        UI actions
     ========================== */
     refresh(opts = {}) {
-        // this.startAjax("listActive", { scope: this.scope, _auto: !!opts.silent }, { silent: !!opts.silent });
+        this.startAjax("listActive", { scope: this.scope, _auto: !!opts.silent }, { silent: !!opts.silent });
     }
 
     render() {
